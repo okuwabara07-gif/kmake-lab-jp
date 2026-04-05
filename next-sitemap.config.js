4824d1491077069bc8df7be1861fedacc6212d1d
okuwabara07-gif <o.kuwabara07@gmail.com>
2026-04-05 16:24:21 +0900

    feat: add next-sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,5 +2,10 @@
 module.exports = {
   siteUrl: 'https://kmake-lab-jp.vercel.app',
   generateRobotsTxt: true,
-  outDir: './public',
-};
+  robotsTxtOptions: {
+    additionalSitemaps: ['https://kmake-lab-jp.vercel.app/sitemap.xml'],
+  },
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000,
+}
